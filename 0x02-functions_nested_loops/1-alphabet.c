6eb790b10a4d43759215d0fb8ecaaacd9bf85d95
Najjashy1 <muhdabdullahi63@yahoo.com>
2023-09-12 11:27:44 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <main.h>
+/**
+ * main - A program that prints with _putchar
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch = 'a';
+	while (ch => 'z')
+	{
+		printf("c% ", ch);
+		ch++;
+	}
+	return (0)
+}
